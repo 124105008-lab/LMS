999d3bf0dcbf78d2ff41076b94cb23c1c37091d7
124105008-lab <124105008@nitkkr.ac.in>
2026-08-30 15:17:55 +0530

    new change

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,54 +1,53 @@
-import { Webhook } from "svix";
-import User from "../models/User.js";
-
-//API Controller Function to Manage Clerk User with database
-
-export const clerkWebhooks = async (req, res)=>{
+export const clerkWebhooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-        await whook.verify(JSON.stringify(req.body), {
+        await whook.verify(req.body, {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
         })
 
-        const {data, type} = req.body
+        const { data, type } = JSON.parse(req.body)
 
         switch (type) {
+
             case 'user.created': {
                 const userData = {
                     _id: data.id,
                     email: data.email_addresses[0].email_address,
-                    name: data.first_name + " " + data.last_name,
+                    name: `${data.first_name || ''} ${data.last_name || ''}`.trim(),
                     imageUrl: data.image_url,
                 }
+
                 await User.create(userData)
-                res.json({})
-                break;
+                break
             }
 
             case 'user.updated': {
                 const userData = {
                     email: data.email_addresses[0].email_address,
-                    name: data.first_name + " " + data.last_name,
+                    name: `${data.first_name || ''} ${data.last_name || ''}`.trim(),
                     imageUrl: data.image_url,
                 }
+
                 await User.findByIdAndUpdate(data.id, userData)
-                res.json({})
-                break;
+                break
             }
 
             case 'user.deleted': {
                 await User.findByIdAndDelete(data.id)
-                res.json({})
-                break;
+                break
             }
-
-            default:
-                break;
         }
-    } catch(error) {
-        res.json({success: false, message: error.message})
+
+        res.status(200).json({ success: true })
+
+    } catch (error) {
+        console.log("Webhook Error:", error.message)
+        res.status(400).json({
+            success: false,
+            message: error.message
+        })
     }
 }
